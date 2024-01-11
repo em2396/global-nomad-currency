257980be23585601252b1a4e034a64c7266c712d
Emalee Poellot <emalee.poellot@gmail.com>
2024-01-11 12:09:51 -0700

    fixBug/Solve async but issue

--- src/App/App.js
+++ src/App/App.js
@@ -9,10 +9,11 @@ import './App.css';
 
 export default function App() {
   const [ currency, setCurrency ] = useState([])
-  const [ conversionRate, setConversionRate] = useState(0);
+  // const [ conversionRate, setConversionRate] = useState(0);
   const [ conversion, setConversion ] = useState(null);
   const [ savedToConversions, setSavedToConversions ] = useState([]); 
   const [ error, setError ] = useState("");
+  const [ isLoading, setIsLoading ] = useState(false);
 
   useEffect(() => {
     getCurrency() 
@@ -31,34 +32,12 @@ export default function App() {
         loop: true,
         renderer: 'svg'
     })
-}, [])
- 
-
-function twoConversions(one, two, amount) {
-    return fetch(`https://v6.exchangerate-api.com/v6/516b2360d76d3364e8dc234b/pair/${one}/${two}`)
-        .then(response => {
-          console.log(response, 'response')
-            if(!response.ok) {
-                console.log('err') 
-            }
-            return response.json()
-        })
-      .then(data => {
-        data = setConversionRate(data.conversion_rate * amount)
-        console.log(data, 'data ')
-        return data
-      })
-    }
-    console.log(conversionRate)
+  }, [])
     
-    function currentConversionDisplay(newConversion, buttonClick) {
-  console.log('Inside currentConversionDisplay');
-  console.log('newConversion:', newConversion);
-  console.log('buttonClick:', buttonClick);
-    setConversion({...newConversion, conversionRate})
+  function currentConversionDisplay(newConversion, buttonClick, getConv) {
+    setConversion({...newConversion})
     if (buttonClick.contains('save-conversion-button')) {
       setSavedToConversions([...savedToConversions, conversion])
-      console.log('saved conversions:',savedToConversions)
     }
   }
 
@@ -78,7 +57,7 @@ function twoConversions(one, two, amount) {
           path="/"
           element={
             <>
-              <Form currentConversionDisplay={currentConversionDisplay} twoConversions={twoConversions} currency={currency}/>
+              <Form currentConversionDisplay={currentConversionDisplay} currency={currency}/>
               <ShowConversion className="current-conversion" conversion={conversion} />
               {error && <h2>Oh No! An error occurred!</h2>}
             </>
--- src/OneConversion/OneConversion.js
+++ src/OneConversion/OneConversion.js
@@ -1,10 +1,14 @@
-
+import './OneConversion.css'
 
 export default function OneConversion({current, deleteSaved, id}) {
     console.log(current, 'current inside one conversion')
     return (
         <div>
-            <button onClick={() => deleteSaved(id)}>Delete</button>
+            <button onClick={() => deleteSaved(id)}>
+                {/* <img src="../minus-circle.svg" alt="delete-button"/>    */}
+                {/* <img src="do_not_disturb_on_FILL0_wght400_GRAD0_opsz24" alt="delete"/> */}
+                
+            </button>
             <h2>{current.amount} {current.firstCountry} to {current.secondCountry} is {current.conversionRate}</h2>
         </div>
     )
